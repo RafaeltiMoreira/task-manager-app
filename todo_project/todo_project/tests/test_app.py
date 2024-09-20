a7b495435298c9d8cfaebb7486c95a37f393dd41
rafaeltimoreirass@gmail.com <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-09-20 02:55:48 -0300

    Corrigindo testes do arquivo test_app

---
+++ todo_project/todo_project/tests/test_app.py
@@ -0,0 +1,11 @@
+import pytest
+from todo_project.run import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
