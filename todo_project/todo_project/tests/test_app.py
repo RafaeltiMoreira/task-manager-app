8d81f218edbc1c3719bd46f4c3e03713a1cf5723
rafaeltimoreirass@gmail.com <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-09-20 05:28:45 -0300

    Mensagem resposta 200

--- todo_project/todo_project/tests/test_app.py
+++ todo_project/todo_project/tests/test_app.py
@@ -8,4 +8,4 @@ def client():
 
 def test_home_page(client):
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 200, "Expected status code 200 but got {}".format(response.status_code)
